c925c412cd40cb1776698f1a07c929eb810a8491
akselheg <akselheg@stud.ntnu.no>
2020-09-25 11:37:02 +0200

    some fix and reorganazing

--- MatlabCode/TestPlot.m
+++ MatlabCode/TestPlot.m
@@ -1,10 +1,36 @@
-clc;close all; clearvars;
-
+clc; close all; clearvars;
+% AbsoluteWind,Depth,DesiredHeading,DesiredPath,DesiredSpeed,DesiredZ,GpsFix,RelativeWind,RemoteSensorInfo
+addpath("./Mausund181204")
 data1 = load('GpsFix.mat');
 data1 = data1.GpsFix;
-data2 = load('Salinity.mat');
-data2 = data2.Salinity;
+data2 = load('DesiredSpeed.mat');
+data2 = data2.DesiredSpeed;
+data3 = load('RelativeWind.mat');
+data3 = data3.RelativeWind;
 
-PlotMraData(data1.timestamp, data1.cog, "Cog",...
-    data1.timestamp, data1.sog , "Sog", ...
-    data2.timestamp, data2.value, 'Salinity');
+PlotMraData(data1.timestamp, data1.sog , "Sog", ...
+    data2.timestamp, data2.value, 'DesiredSpeed');
+
+PlotMraData(data1.timestamp, data1.sog , "Sog", ...
+    data3.timestamp, deg2rad(data3.angle), 'angle');
+
+PlotMraData(data1.timestamp, data1.sog , "Sog", ...
+    data3.timestamp, data3.speed, 'speed');
+
+rmpath("./Mausund181204")
+addpath("./Mausund221241")
+data1 = load('GpsFix.mat');
+data1 = data1.GpsFix;
+data2 = load('DesiredSpeed.mat');
+data2 = data2.DesiredSpeed;
+data3 = load('RelativeWind.mat');
+data3 = data3.RelativeWind;
+
+PlotMraData(data1.timestamp, data1.sog , "Sog", ...
+    data2.timestamp, data2.value, 'DesiredSpeed');
+
+PlotMraData(data1.timestamp, data1.sog , "Sog", ...
+    data3.timestamp, deg2rad(data3.angle), 'angle');
+
+PlotMraData(data1.timestamp, data1.sog , "Sog", ...
+    data3.timestamp, data3.speed, 'speed');
