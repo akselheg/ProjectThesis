6bf0527bf53ac1649339730411db4568c6de2968
Aksel Heggernes <akselheg@stud.ntnu.no>
2020-09-25 13:18:21 +0200

    some wheather testing

---
+++ MatlabCode/plotCurrent.m
@@ -0,0 +1,11 @@
+close all; clearvars;clc;
+x = -200*pi:pi*20:200*pi;
+y = x;
+
+hold on
+
+[X, Y] = meshgrid(x,y);
+
+[xCurrent,yCurrent] = xyCurrent(X,Y);
+figure(3)
+quiver(X,Y,xCurrent,yCurrent);
