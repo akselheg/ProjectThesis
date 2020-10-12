794cccb51eb9ec0226a5e24819298bfb2b00fa46
Aksel Heggernes <akselheg@stud.ntnu.no>
2020-10-12 13:24:15 +0200

    blabla

--- MatlabCode/AWindInfo.m
+++ MatlabCode/AWindInfo.m
@@ -11,9 +11,20 @@ gpsData.timestamp = gpsData.timestamp - gpsData.timestamp(1);
 windData.timestamp = windData.timestamp - windData.timestamp(1);
 windDataAngles = interp1(windData.timestamp, windData.angle,gpsData.timestamp);
 windDataSpeed = interp1(windData.timestamp, windData.speed,gpsData.timestamp);
+windAnglesSmooth = [];
+SogSmooth = [];
+windSpeedSmooth = [];
+sample = 5;
+for i = sample + 1 : length(windDataAngles) - sample
+    windAnglesSmooth = cat(1,windAnglesSmooth, mean(windDataAngles(i-sample:i+sample)));
+    SogSmooth = cat(1,SogSmooth, mean(gpsData.sog(i-sample:i+sample)));
+    windSpeedSmooth = cat(1,windSpeedSmooth, mean(windDataSpeed(i-sample:i+sample)));
+end
+figure;
+plot(SogSmooth)
 for i = 1 : length(windDataAngles)
     if windDataAngles(i) > 180
         %windDataAngles(i) = windDataAngles(i) - 360;
     end
 end
-scatter3(windDataAngles(1:10:end),windDataSpeed(1:10:end),gpsData.sog(1:10:end))
+%scatter3(windDataAngles(1:10:end),windDataSpeed(1:10:end),gpsData.sog(1:10:end))
