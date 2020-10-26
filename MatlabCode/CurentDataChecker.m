8bc15e635cbbd85808126c2eb7334685ee81a9d4
Aksel Heggernes <akselheg@stud.ntnu.no>
2020-10-26 18:32:17 +0100

    working current download and better position finder in analysis

--- MatlabCode/CurentDataChecker.m
+++ MatlabCode/CurentDataChecker.m
@@ -1,11 +1,11 @@
+flag = false;
 for i = 1:2602
     for j = 1:902
         for k = 1:24
-            if currentNorth(i,j,1,k) ~= 0
-                disp([num2str(i),num2str(j),num2str(k)])
+            if currentNorth(i,j,k) ~= 0
+                flag = true;
             end
-            if currentEast(i,j,1,k) ~= 0
-                disp([num2str(i),num2str(j),num2str(k)])
+            if currentEast(i,j,k) ~= 0
             end
         end
     end
--- MatlabCode/MausundAnalysis.m
+++ MatlabCode/MausundAnalysis.m
@@ -23,9 +23,13 @@ relWaveDirToCog_data = [];
 relWindDirToCog_data = [];
 ForecastWaveFreq_data = [];
 ForcastWindSpeed_data = [];
+NorthCurrent_data = [];
+EastCurrent_data = [];
+
 
 avrager = 4*60; % average over x min
 for i = 1: 9
+    disp('Loading new data')
     %% load data
     if i == 1
         path = './Mausund200701_181204/';
@@ -35,6 +39,7 @@ for i = 1: 9
         EulerAngles = load('EulerAngles.mat');
         rmpath(path)
         load('weatherData_2020-7-1_2020-7-2.mat')
+        load('currentweatherData_2020-7-1_2020-7-3.mat')
         disp('Done loading data')
     end
     if i == 2
@@ -54,6 +59,7 @@ for i = 1: 9
         EulerAngles = load('EulerAngles.mat');
         rmpath(path)
         load('weatherData_2020-7-3_2020-7-4.mat')
+        load('currentweatherData_2020-7-3_2020-7-4.mat')
         disp('Done loading data')
     end
     if i == 4
@@ -80,6 +86,7 @@ for i = 1: 9
         gpsFix = load('GpsFix.mat');
         RelativeWind = load('RelativeWind.mat');
         EulerAngles = load('EulerAngles.mat');
+        load('currentweatherData_2020-7-4_2020-7-5.mat')
         rmpath(path)
         disp('Done loading data')
     end
@@ -91,6 +98,7 @@ for i = 1: 9
         EulerAngles = load('EulerAngles.mat');
         rmpath(path)
         load('weatherData_2020-7-5_2020-7-5.mat')
+        load('currentweatherData_2020-7-5_2020-7-5.mat')
         disp('Done loading data')
     end
     if i == 8
@@ -101,6 +109,7 @@ for i = 1: 9
         EulerAngles = load('EulerAngles.mat');
         rmpath(path)
         load('weatherData_2020-7-6_2020-7-6.mat')
+        load('currentweatherData_2020-7-6_2020-7-6.mat')
         disp('Done loading data')
     end
      if i == 9
@@ -111,6 +120,7 @@ for i = 1: 9
         EulerAngles = load('EulerAngles.mat');
         rmpath(path)
         load('weatherData_2020-7-9_2020-7-9.mat')
+        load('currentweatherData_2020-7-9_2020-7-9.mat')
         disp('Done loading data')
     end
     %% Format and interpolations
@@ -121,20 +131,20 @@ for i = 1: 9
     messuredRelWindDir = interp1(windData.timestamp, ssa(windData.angle,'deg' ),gps_data.timestamp);
     messuredRelWindSpeed = interp1(windData.timestamp, windData.speed,gps_data.timestamp);
     [latSize,longSize] = size(latitudeMapWave);
+    [curlatSize,curlongSize] = size(latitudeCurrentMap);
     first = true;
-
     x = 0;
     y = 0;
     disp('Done formating')
     disp('Start running through data')
     %% run
-    for min = (2*avrager) : length(gps_data.sog) - (2*avrager)
-        if ~mod(gps_data.utc_time(min),avrager)
-            curr_hour = floor(double(gps_data.utc_time(min))/3600) ...
-                + 24*(double(gps_data.utc_day(min)-gps_data.utc_day(1)));
+    for m = (2*avrager) : length(gps_data.sog) - (2*avrager)
+        if ~mod(gps_data.utc_time(m),avrager)
+            curr_hour = floor(double(gps_data.utc_time(m))/3600) ...
+                + 24*(double(gps_data.utc_day(m)-gps_data.utc_day(1)));
 
-            lat = mean(rad2deg(gps_data.lat(min-avrager:min+avrager)));
-            lon = mean(rad2deg(gps_data.lon(min-avrager:min+avrager)));
+            lat = mean(rad2deg(gps_data.lat(m-avrager:m+avrager)));
+            lon = mean(rad2deg(gps_data.lon(m-avrager:m+avrager)));
 
 
             increment = 0.0001; % resolution lat and lon in gps
@@ -142,30 +152,24 @@ for i = 1: 9
             indexes_found = false;
 
             % Find closest
-            while ~indexes_found
-                for ii = 1:latSize
-                    for jj = 1:longSize
-                        if abs(latitudeMapWave(ii, jj) - lat) <= num_increments*increment ...
-                                && abs(longitudeMapWave(ii,jj) - lon) <= num_increments*increment
-                            indexes_found = true;
-                            x = ii;
-                            y = jj;
-                        end
-                    end
-                end
-                num_increments = num_increments + 1;
-            end
+            error_map = sqrt((latitudeMapWave - lat).^2 + (longitudeMapWave - lon).^2);
+            [x,y] = find(error_map == min(error_map, [], 'all'));
+            
+            error_map = sqrt((latitudeCurrentMap - lat).^2 + (longitudeCurrentMap - lon).^2);
+            [xcurrent,ycurrent] = find(error_map == min(error_map, [], 'all'));
 
-            cog = rad2deg(mean(gps_data.cog(min-avrager:min+avrager)));
-            psi = rad2deg(mean(EulerAngles.psi(min-avrager:min+avrager)));
-            sog = mean(gps_data.sog(min-avrager:min+avrager));
+            cog = rad2deg(mean(gps_data.cog(m-avrager:m+avrager)));
+            psi = rad2deg(mean(EulerAngles.psi(m-avrager:m+avrager)));
+            sog = mean(gps_data.sog(m-avrager:m+avrager));
             curWaveDir = ssa(waveDir(x,y,curr_hour+1),'deg');
             curWindDir = ssa(windDir(x,y,curr_hour+1),'deg');
+            
+            curNorthCur = currentNorth(xcurrent,ycurrent,curr_hour+1);
+            curEastCur = currentEast(xcurrent,ycurrent,curr_hour+1);
+            
             ForcastWindSpeed = windSpeed(x,y,curr_hour + 1);
-            curMessuredRelWindDir = mean(messuredRelWindDir(min-avrager:min+avrager));
-            curMessuredRelWindSpeed = mean(messuredRelWindSpeed(min-avrager:min+avrager));
-
-
+            curMessuredRelWindDir = mean(messuredRelWindDir(m-avrager:m+avrager));
+            curMessuredRelWindSpeed = mean(messuredRelWindSpeed(m-avrager:m+avrager));
             if waveSize(x, y, curr_hour + 1) < 0.001
                 ForecastWaveSize_data = cat(1, ForecastWaveSize_data, ForecastWaveSize_data(end));
             else
@@ -179,6 +183,8 @@ for i = 1: 9
             psi_data = cat(1, psi_data,psi);
             sog_data = cat(1, sog_data,sog);
             waveDir_data = cat(1, waveDir_data, ssa(psi+curWaveDir, 'deg'));
+            NorthCurrent_data = cat(1, NorthCurrent_data, curNorthCur);
+            EastCurrent_data = cat(1, EastCurrent_data, curEastCur);
             relWaveDir_data = cat(1, relWaveDir_data, ssa(ssa(curWaveDir + 180,'deg') - psi , 'deg'));
             relWindDir_data = cat(1, relWindDir_data, ssa(ssa(curWindDir + 180,'deg') - psi , 'deg'));
             relWaveDirToCog_data = cat(1, relWaveDirToCog_data, ssa(ssa(curWaveDir + 180,'deg') - cog , 'deg'));
@@ -188,8 +194,8 @@ for i = 1: 9
             messuredRelWindSpeed_data = cat(1, messuredRelWindSpeed_data, curMessuredRelWindSpeed);
             ForcastWindSpeed_data = cat(1, ForcastWindSpeed_data, ForcastWindSpeed);
 
-            if ~mod(gps_data.utc_time(min),3600) || first
-                str = sprintf('| Day: %d \t | Hour: %d \t |', ...
+            if ~mod(gps_data.utc_time(m),3600) || first
+                str = sprintf('| Day: %d  | Hour: %d \t|', ...
                     (floor(curr_hour/24)+1) + gps_data.utc_day(1)-1, (mod(curr_hour,24)));
                 disp(str)
                 first = false;
@@ -204,17 +210,19 @@ nninputs = [ForecastWaveSize_data relWaveDir_data relWindDir_data ...
 %%
 disp('Plotting Data')
 figure(1)
-scatter3(relWaveDir_data,ForecastWaveSize_data,sog_data)
-% for i = 1:length(relWaveDir_data)
-%     if (ForecastWaveFreq_data(i) < 8)
-%         scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'b')
-%     elseif (ForecastWaveFreq_data(i) < 9)
-%         scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'r')
-%     else
-%         scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'g')
-%     end
-%     hold on
-% end
+%scatter3(relWaveDir_data,ForecastWaveSize_data,sog_data)
+for i = 1:length(relWaveDir_data)
+    if (ForecastWaveFreq_data(i) < 6)
+        scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'b')
+    elseif (ForecastWaveFreq_data(i) < 7)
+        scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'r')
+    elseif (ForecastWaveFreq_data(i) < 8)
+        scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'g')
+    else
+        scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'k')
+    end
+    hold on
+end
 xlabel 'Relative wave direction',ylabel 'Wave Size',zlabel 'SOG';
 hold off
 figure(4)
