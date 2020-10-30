0058cd5c2f0522d210ffc4df072dbb87cde542ac
akselheg <akselheg@stud.ntnu.no>
2020-10-30 12:22:53 +0100

    Analysis finnished and sent to veileder

--- MatlabCode/MausundAnalysis.m
+++ MatlabCode/MausundAnalysis.m
@@ -30,7 +30,7 @@ CurrentSpeed_data = [];
 speed_data = [];
 
 
-avrager = 4*60; % average over x min
+avrager = 2*60; % average over x min
 for i = 1: 9
     disp('Loading new data')
     %% load data
@@ -176,16 +176,15 @@ for i = 1: 9
             % Current vector at given time and position
             currentNorthCur = currentNorth(xcurrent,ycurrent,curr_hour+1);
             currentEastCur = currentEast(xcurrent,ycurrent,curr_hour+1);
-            current_vector = [currentEastCur; currentNorthCur];
+            current_vector = [currentNorthCur; currentEastCur];
             
             % Velocity vector of the vessel
             Sog_vector = [sog*cos(deg2rad(cog)); sog*sin(deg2rad(cog))];
             Velocity_vector = Sog_vector - current_vector;
             speed = norm(Velocity_vector);
             speed_data = cat(1,speed_data,speed);
-            a = [1;0];
             % Angle between velocity and current direction
-            currentDir = rad2deg(atan2(current_vector(2), current_vector(1)) - atan2(a(2), a(1)));
+            currentDir = (atan2d(current_vector(2), current_vector(1)));
             % magnitude of the current
             currentSpeed = norm(current_vector);
             
@@ -206,18 +205,17 @@ for i = 1: 9
             cog_data = cat(1, cog_data,cog);
             psi_data = cat(1, psi_data,psi);
             sog_data = cat(1, sog_data,sog);
-            waveDir_data = cat(1, waveDir_data, ssa(psi+curWaveDir, 'deg'));
             NorthCurrent_data = cat(1, NorthCurrent_data, currentNorthCur);
             EastCurrent_data = cat(1, EastCurrent_data, currentEastCur);
-            relWaveDir_data = cat(1, relWaveDir_data, ssa(ssa(curWaveDir + 180,'deg') - psi , 'deg'));
-            relWindDir_data = cat(1, relWindDir_data, ssa(ssa(curWindDir + 180,'deg') - psi , 'deg'));
-            relWaveDirToCog_data = cat(1, relWaveDirToCog_data, ssa(ssa(curWaveDir + 180,'deg') - cog , 'deg'));
-            relWindDirToCog_data = cat(1, relWindDirToCog_data, ssa(ssa(curWindDir + 180,'deg') - cog , 'deg'));
+            relWaveDir_data = cat(1, relWaveDir_data, ssa(cog - curWaveDir - 180 , 'deg'));
+            relWindDir_data = cat(1, relWindDir_data, ssa(psi - curWindDir - 180 , 'deg'));
+            relWaveDirToCog_data = cat(1, relWaveDirToCog_data, ssa(ssa(cog-curWaveDir,'deg') - 180 , 'deg'));
+            relWindDirToCog_data = cat(1, relWindDirToCog_data, ssa(ssa(cog-curWindDir ,'deg') - 180 , 'deg'));
             ForecastWindDir_data = cat(1, ForecastWindDir_data, curWindDir);
             messuredRelWindDir_data = cat(1, messuredRelWindDir_data, curMessuredRelWindDir);
             messuredRelWindSpeed_data = cat(1, messuredRelWindSpeed_data, curMessuredRelWindSpeed);
             ForcastWindSpeed_data = cat(1, ForcastWindSpeed_data, ForcastWindSpeed);
-            CurrentDir_data = cat(1, CurrentDir_data, ssa(currentDir -psi, 'deg'));
+            CurrentDir_data = cat(1, CurrentDir_data, ssa(cog- currentDir, 'deg'));
             CurrentSpeed_data = cat(1, CurrentSpeed_data, currentSpeed);
             
             if ~mod(gps_data.utc_time(m),3600) || first
@@ -241,30 +239,33 @@ nninputs =  [relWaveDir_data relWindDir_data  ForcastWindSpeed_data ...
 %%
 disp('Plotting Data')
 figure;
-%scatter3(relWaveDir_data,ForecastWaveSize_data,sog_data)
-for i = 1:length(relWaveDir_data)
-    if (ForecastWaveFreq_data(i) < 6)
-        scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'b')
-    elseif (ForecastWaveFreq_data(i) < 7)
-        scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'r')
-    elseif (ForecastWaveFreq_data(i) < 8)
-        scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'g')
-    else
-        scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'k')
-    end
-    hold on
-end
+scatter3(abs(relWaveDir_data),ForecastWaveSize_data,sog_data)
+% for i = 1:length(relWaveDir_data)
+%     if (ForecastWaveFreq_data(i) < 6)
+%         scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'b')
+%     elseif (ForecastWaveFreq_data(i) < 7)
+%         scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'r')
+%     elseif (ForecastWaveFreq_data(i) < 8)
+%         scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'g')
+%     else
+%         scatter3(relWaveDir_data(i),ForecastWaveSize_data(i),sog_data(i), 'k')
+%     end
+%     hold on
+% end
 xlabel 'Relative wave direction',ylabel 'Wave Size',zlabel 'SOG';
 hold off
 figure;
-scatter3(relWaveDir_data,ForecastWaveFreq_data,sog_data)
+scatter3(abs(relWaveDir_data),ForecastWaveFreq_data,sog_data)
 xlabel 'Relative wave direction',ylabel 'Wave period',zlabel 'SOG';
 figure;
 scatter3(relWaveDir_data,relWindDir_data, sog_data)
 xlabel 'Relative wave direction',ylabel 'Relative Wind Angle',zlabel 'SOG';
 figure
-scatter3(messuredRelWindSpeed_data,messuredRelWindDir_data, sog_data)
+scatter3(ForcastWindSpeed_data,abs(relWindDir_data), sog_data)
 xlabel 'Wind Speed',ylabel 'Relative Wind Angle',zlabel 'SOG';
+figure
+scatter3(messuredRelWindSpeed_data,abs(messuredRelWindDir_data), sog_data)
+xlabel 'Wind Speed',ylabel 'Relative Wind Angle',zlabel 'SOG'
 figure
 scatter3(ForecastWaveSize_data,ForecastWaveFreq_data, sog_data)
 xlabel 'Wave Size',ylabel 'Wave period',zlabel 'SOG';
@@ -286,6 +287,20 @@ for i = 1 : length(sog_data)
 end
 xlabel 'Wave Size',ylabel 'Wave period',zlabel 'SOG';
 legend('<45', '<135','<180')
+figure;
+count = 0;
+for i = 1 : length(sog_data)
+    if ForecastWaveFreq_data(i) < 7.5 && ForecastWaveSize_data(i) > 2.5...
+            && messuredRelWindSpeed_data(i) > 3 && abs(messuredRelWindDir_data(i)) > 120 ...
+            && CurrentSpeed_data(i) < 0.2
+        plot(count,sog_data(i), 'o')
+        count = count +1;
+        hold on
+    end
+end
+ylabel 'SOG' ; xlabel 'Sample'; title 'Speed at desired environment'
+hold off
+%% 
 disp('Doing Neural')
 [net,perform, netError, netTrainState] = neuralNet(double(nninputs)',double(sog_data)');
 for i = 1:10
